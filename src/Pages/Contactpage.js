7b64f58d50e7dd4b04f7d42182082455869a0122
Bhanu-Partap <bhanupartap25275@gmail.com>
2022-08-03 16:32:11 +0530

    3

---
+++ src/Pages/Contactpage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Contactpage() {
+  return (
+    <section id='contact'>
+        <div className="main container"  xs={{order:"last"}}>
+            <h1 className="display-1 fw-normal ">Let's discuss </h1>
+            <h1 className="display-1 fw-normal ">Your Project</h1>
+            <hr />
+            </div>
+
+            <div className="form">
+                formgroup
+            </div>
+
+    </section>
+  )
+}
